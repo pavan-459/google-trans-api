98aa483c86d7f8e2b3d21de4247ebe76b5913b43
pavan-459 <pavan459@protonmail.ch>
2023-03-17 08:56:30 +0530

    api integration

---
+++ server.js
@@ -0,0 +1,63 @@
+
+const PORT = 8000
+const axios = require('axios').default
+const express = require('express')
+const cors = require('cors')
+require('dotenv').config()
+const app = express()
+
+app.use(cors())
+
+app.get('/languages', async function (req, res) {
+    const options = {
+      method: 'GET',
+      headers: {
+        'x-rapidapi-host': process.env.RAPID_API_HOST,
+        'x-rapidapi-key': process.env.RAPID_API_KEY,
+      },
+    }
+
+    try {
+      const response = await axios(
+        'https://google-translate1.p.rapidapi.com/language/translate/v2/languages',
+        options
+      )
+      const arrayOfData = Object.keys(response.data.data).map(
+        (key) => response.data.data[key]
+      )
+      res.status(200).json(arrayOfData)
+    } catch (err) {
+      console.log(err)
+      res.status(500).json({ message: err })
+    }
+  })
+
+app.get('/translation', async (req, res) => {
+    const { textToTranslate, outputLanguage, inputLanguage } = req.query
+
+    const options = {
+      method: 'GET',
+      params: {
+        text: textToTranslate,
+        tl: outputLanguage,
+        sl: inputLanguage,
+      },
+      headers: {
+        'x-rapidapi-host': process.env.RAPID_API_HOST,
+        'x-rapidapi-key': process.env.RAPID_API_KEY,
+      },
+    }
+
+    try {
+      const response = await axios(
+        'https://google-translate1.p.rapidapi.com/language/translate/v2',
+        options
+      )
+      res.status(200).json(response.data.data.translation)
+    } catch (err) {
+      console.log(err)
+      res.status(500).json({ message: err })
+    }
+  })
+
+app.listen(PORT, () => console.log('Server running on port ' + PORT))
--- src/App.js
+++ src/App.js
@@ -1,25 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react'
+import TextBox from './components/textbox'
+import Modal from './components/modal'
+import Arrows from './components/arrows'
+import Button from './components/button'
+import axios from 'axios'
 
-function App() {
+const App = () => {
+  const [showModal, setShowModal] = useState(false)
+  const [languages, setLanguages] = useState([])
+  const [inputLanguage, setInputLanguage] = useState('English')
+  const [outputLanguage, setOutputLanguage] = useState('Polish')
+  const [textToTranslate, setTextToTranslate] = useState('')
+  const [translatedText, setTranslatedText] = useState('')
+
+
+  const getLanguages = async () => {
+    const response = await axios.get('https://localhost:8000/languages')
+    setLanguages(response.data)
+  }
+  useEffect(() => {
+    getLanguages()
+  }, [])
+
+  const translate = async () => {
+    console.log('translate')
+    const data = {
+      textToTranslate, outputLanguage, inputLanguage
+    }
+    const response = await axios.get('https://localhost:8000/translation', {
+      params : data
+    })
+    console.log('response', response)
+    setTranslatedText(response.data)
+  }
+
+  const handleClick = () => {
+    setInputLanguage(outputLanguage)
+    setOutputLanguage(inputLanguage)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      {!showModal && (
+        <>
+          <TextBox
+            // eslint-disable-next-line react/style-prop-object
+            style="input"
+            setShowModal={setShowModal}
+            selectedLanguage={inputLanguage}
+            setTextToTranslate={setTextToTranslate}
+            textToTranslate={textToTranslate}
+            setTranslatedText={setTranslatedText}
+          />
+          <div className="arrow-container" onClick={handleClick}>
+            <Arrows />
+          </div>
+          <TextBox
+            // eslint-disable-next-line react/style-prop-object
+            style="output"
+            setShowModal={setShowModal}
+            selectedLanguage={outputLanguage}
+            translatedText={translatedText}
+          />
+          <div className="button-container" onClick={translate}>
+            <Button />
+          </div>
+        </>
+      )}
+      {showModal && (
+        <Modal
+          showModal={showModal}
+          setShowModal={setShowModal}
+          languages={languages}
+          chosenLanguage={
+            showModal === 'input' ? inputLanguage : outputLanguage
+          }
+          setChosenLanguage={
+            showModal === 'input' ? setInputLanguage : setOutputLanguage
+          }
+        />
+      )}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
